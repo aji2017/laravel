60841cbcc9faa8b064e15b1f40dd7ee3bd0653ea
aji2017 <muhammadajipangestu86@gmail.com>
2023-09-13 19:22:16 +0800

    pertama commit

---
+++ resources/views/dashboard/baru.blade.php
@@ -0,0 +1,14 @@
+@extends('app')
+
+@section('title', 'baru')
+
+@section('content')
+<section class="section">
+          <div class="section-header">
+            <h1>Halaman Baru</h1>
+          </div>
+
+          <div class="section-body">
+          </div>
+        </section>
+@endsection
