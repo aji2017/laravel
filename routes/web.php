e9e1358ef429280388682beb988f6322b7ca6de9
aji2017 <muhammadajipangestu86@gmail.com>
2023-09-13 18:55:15 +0800

    pertama commit

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard.home');
+});
+
+Route::get('/baru', function () {
+    return view('dashboard.baru');
 });
