ae9ba2b8c2795cfe924efc25ab1c072f8cbe9d81
aji2017 <muhammadajipangestu86@gmail.com>
2023-09-14 01:07:32 +0800

    ketiga commit

--- resources/views/auth/resetpass.blade.php
+++ resources/views/auth/resetpass.blade.php
@@ -7,7 +7,7 @@
             </div>
 
             <div class="card card-primary">
-              <div class="card-header"><h4>Forgot Password</h4></div>
+              <div class="card-header"><h4>Reset Password</h4></div>
 
               <div class="card-body">
                 <p class="text-muted">We will send a link to reset your password</p>
@@ -18,8 +18,22 @@
                   </div>
 
                   <div class="form-group">
+                    <label for="password">New Password</label>
+                    <input id="password" type="password" class="form-control pwstrength" data-indicator="pwindicator" name="password" tabindex="2" required>
+                    <div id="pwindicator" class="pwindicator">
+                      <div class="bar"></div>
+                      <div class="label"></div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="password-confirm">Confirm Password</label>
+                    <input id="password-confirm" type="password" class="form-control" name="confirm-password" tabindex="2" required>
+                  </div>
+
+                  <div class="form-group">
                     <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
-                      Forgot Password
+                      Reset Password
                     </button>
                   </div>
                 </form>
--- routes/web.php
+++ routes/web.php
@@ -15,20 +15,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('dashboard.home');
-});
+})->name('home');
 
 Route::get('/baru', function () {
     return view('dashboard.baru');
-});
+})->name('baru');
 
 Route::get('/login', function () {
     return view('auth.login');
-});
+})->name('login');
 
 Route::get('/register', function () {
     return view('auth.register');
-});
+})->name('register');
 
 Route::get('/reset', function () {
     return view('auth.reset');
-});
+})->name('reset');
+
+Route::get('/resetpass', function () {
+    return view('auth.resetpass');
+})->name('resetpass');
